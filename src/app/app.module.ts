20ded2b13f5d77258f8f9d6b9a4648b3f1b26077
231189alo <231189@ids.upchiapas.edu.mx>
2024-10-02 08:00:34 -0600

    Inicialización del proyecto: se crearon los archivos base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ProductListComponent } from './features/products/product-list/product-list.component';
+import { FormProductComponent } from './features/products/form-product/form-product.component';
+import { OfertsListComponent } from './features/oferts/oferts-list/oferts-list.component';
+import { OfertsFormComponent } from './features/oferts/oferts-form/oferts-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SidebarComponent,
+    ProductListComponent,
+    FormProductComponent,
+    OfertsListComponent,
+    OfertsFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/features/oferts/oferts-list/oferts-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-oferts-list',
+  templateUrl: './oferts-list.component.html',
+  styleUrl: './oferts-list.component.css'
+})
+export class OfertsListComponent {
+
+}
